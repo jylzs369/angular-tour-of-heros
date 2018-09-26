773867486c7a4cce89dabb790d885a64e6c5893b
Ushio <jylzs369@163.com>
2018-09-26 16:58:42 +0800

    添加组件

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,10 +1,11 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import Loki from 'lokijs'
+import { Hero } from './hero';
+// import Loki from 'lokijs'
 
-const db = new Loki('loki.json')
-let collectionHeroes = db.getCollection('heroes') || db.addCollection('heroes')
+// const db = new Loki('loki.json')
+// let collectionHeroes = db.getCollection('heroes') || db.addCollection('heroes')
 
-const heroesList = [
+/* const heroesList = [
   { id: 11, name: 'Mr. Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
@@ -15,7 +16,7 @@ const heroesList = [
   { id: 18, name: 'Dr IQ' },
   { id: 19, name: 'Magma' },
   { id: 20, name: 'Tornado' }
-]
+] 
 
 if (!collectionHeroes.count()) {
   heroesList.forEach(item => {
@@ -23,11 +24,27 @@ if (!collectionHeroes.count()) {
   })
 }
 console.log(db)
-
+*/
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const heroes = db.getCollection('heroes').find()
-    console.log(heroes)
+    /* const heroes = db.getCollection('heroes').find()
+    console.log(heroes) */
+    
+    const heroes = [
+      { id: 11, name: 'Mr. Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
     return {heroes};
+  }
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
   }
 }
